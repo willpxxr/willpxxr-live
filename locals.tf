2b2991b27dbe5fd00191a6651f791f2526e6371b
Will Parr <williamparr96@outlook.com>
2024-11-12 12:24:34 +0000

    fix: update record name

--- locals.tf
+++ locals.tf
@@ -16,7 +16,7 @@ locals {
       value   = local.proxied_a_record_placeholder
       proxied = true
     }
-    "_ens" = {
+    "@" = {
       type    = "TXT"
       value   = "ENS1 dnsname.ens.eth 0x20373F5a3Bb30b528a9acdAbE02a3f99fb74ee45"
       proxied = false
