494793c3b1121eb49d36d78896de52c4916fd4b6
Will Parr <the@willpxxr.com>
2025-05-30 00:37:35 +0100

    chore: update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
 variable "cloudflare_api_token" {
+  sensitive   = true
   description = "Cloudflare API Token"
+}
+
+variable "oci_rsa_private_key_base64enc" {
+  sensitive   = true
+  description = "OCI API Key RSA Private Key"
 }
