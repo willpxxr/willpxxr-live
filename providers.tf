87214c6a084f7d7dd5652c09bbe6d5cc4bc2bab3
Will Parr <the@willpxxr.com>
2025-05-30 00:45:31 +0100

    fix: var reference

--- providers.tf
+++ providers.tf
@@ -19,7 +19,7 @@ provider "cloudflare" {
 provider "oci" {
   tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaal7ioy4xx4zw4g2fhbxrcbvkdzuea2t4gm3gbi7jayibgkk55amua"
   user_ocid    = "ocid1.user.oc1..aaaaaaaa6q7pgadu7l4mxnxa5dwmf3sayhvc254u7mtpxezqfhi56yaibc4a"
-  private_key  = base64decode(oci_rsa_private_key_base64enc)
+  private_key  = base64decode(var.oci_rsa_private_key_base64enc)
   fingerprint  = "0a:78:a5:18:2e:4f:1a:a1:83:91:2b:93:51:ff:03:fe"
   region       = "uk-london-1"
 }
