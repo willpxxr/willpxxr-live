ea20a4b7302a660d0e494b354deda5ebd4d6bf3b
Will Parr <the@willpxxr.com>
2025-05-30 11:03:05 +0100

    chore: only create in 2

--- oci.tf
+++ oci.tf
@@ -131,11 +131,6 @@ resource "oci_containerengine_node_pool" "k8s_node_pool" {
 
   node_config_details {
     placement_configs {
-      availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
-      subnet_id           = oci_core_subnet.vcn_private_subnet.id
-    }
-
-    placement_configs {
       availability_domain = data.oci_identity_availability_domains.ads.availability_domains[1].name
       subnet_id           = oci_core_subnet.vcn_private_subnet.id
     }
